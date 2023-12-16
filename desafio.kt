e8a6d90c4ac823c94496f718d583b916b245eb20
SauloCouto <143052328+DevSauloCouto@users.noreply.github.com>
2023-12-16 13:32:34 -0300

    desafio de projeto concluído

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,40 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANÇADO }
 
-class Usuario
+data class User(val name: String, val email: String, var formations: MutableList<Formation>?)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ContentEducational(var name: String, val stack: String, val duration: Int, val level: Nivel)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formation(val name: String, var contents: List<ContentEducational>) {
 
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    val subscribes = mutableListOf<User>()
+
+    fun enroll(vararg users: User) {
+        for(user in users){
+            subscribes.add(user);
+            user.formations = mutableListOf(this);
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val user1 = User("Saulo", "sl@teste", null);
+    val user2 = User("Artur", "artur@teste", null);
+    val user3 = User("Gabriele", "gabi@teste", null);
+    val user4 = User("Couto", "couto@teste", null);
+    val user5 = User("Beatriz", "bia@teste", null);
+
+
+    val contentEducational1 = ContentEducational("Lógica de Programação c/ Java", "JAVA", 45, Nivel.BASICO);
+    val contentEducational2 = ContentEducational("APIs JAVA", "JAVA", 75, Nivel.INTERMEDIARIO);
+    val contentEducational3 = ContentEducational("Java c/ Spring Boot", "JAVA", 90, Nivel.AVANÇADO);
+
+    val formation = Formation("Back-end", listOf(contentEducational1,contentEducational2,contentEducational3));
+    formation.enroll(user1,user2,user3,user4,user5);
+
+    for(user in formation.subscribes){
+        println(user);
+    }
+
 }
